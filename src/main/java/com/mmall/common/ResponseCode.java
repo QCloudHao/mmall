37029e0c16a88ce9cb4883fb377e21a205f49bf4
QCloudHao <499930858@qq.com>
2019-02-08 20:10:12 +0800

    complete User module

--- src/main/java/com/mmall/common/ResponseCode.java
+++ src/main/java/com/mmall/common/ResponseCode.java
@@ -3,5 +3,24 @@ package com.mmall.common;
 /**
  * Created by qyh on 2019/2/6.
  */
-public class ResponseCode {
+public enum ResponseCode {
+    SUCCESS(0,"SUCCESS"),
+    ERROR(1,"ERROR"),
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
+
+    private final int code;
+    private final String desc;
+
+    ResponseCode(int code,String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+    public String getDesc(){
+        return desc;
+    }
 }
