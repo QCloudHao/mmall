7e40da2bd4cb12f4c58503cc879e25d7afd22489
QCloudHao <499930858@qq.com>
2019-02-10 20:13:11 +0800

    complete Classification Management Module

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,7 +1,19 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
 /**
  * Created by qyh on 2019/2/10.
  */
 public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
 }
