63a392980e545d74c0dd3199fcf868ef8da9e8f5
QCloudHao <499930858@qq.com>
2019-02-13 16:18:57 +0800

    complete Commodity Management Module

--- src/main/java/com/mmall/service/IFileService.java
+++ src/main/java/com/mmall/service/IFileService.java
@@ -1,7 +1,10 @@
 package com.mmall.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * Created by qyh on 2019/2/12.
  */
 public interface IFileService {
+    public String upload(MultipartFile file, String path);
 }
