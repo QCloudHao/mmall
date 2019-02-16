a4481a0c49e0576afe9d57565f26a805979bebf3
QCloudHao <499930858@qq.com>
2019-02-16 19:50:58 +0800

    complete Shopping Cart Module

---
+++ src/main/java/com/mmall/service/ICartService.java
@@ -0,0 +1,21 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.vo.CartVo;
+
+/**
+ * Created by qyh on 2019/2/14.
+ */
+public interface ICartService {
+    ServerResponse<CartVo> add(Integer userId, Integer productId, Integer count);
+
+    ServerResponse<CartVo> update(Integer userId,Integer productId,Integer count);
+
+    ServerResponse<CartVo> deleteProduct(Integer userId,String productIds);
+
+    ServerResponse<CartVo> list(Integer userId);
+
+    ServerResponse<CartVo> selectOrUnselect(Integer userId, Integer productId, Integer checked);
+
+    ServerResponse<Integer> getCartProductCount(Integer userId);
+}
