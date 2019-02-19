139fa21d77d9b40256046f4429e8198ba038322b
QCloudHao <499930858@qq.com>
2019-02-19 15:20:17 +0800

    change the version of some jar packages

---
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -0,0 +1,7 @@
+package com.mmall.controller.portal;
+
+/**
+ * Created by qyh on 2019/2/18.
+ */
+public class OrderController {
+}
---
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,7 @@
+package com.mmall.service;
+
+/**
+ * Created by qyh on 2019/2/18.
+ */
+public interface IOrderService {
+}
