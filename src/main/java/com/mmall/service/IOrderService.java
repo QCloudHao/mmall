b65ed643e296a14dc71bac6811fcf3bff0dec037
QCloudHao <499930858@qq.com>
2019-02-19 15:24:08 +0800

    complete Payment Module

--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -1,7 +1,16 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
 /**
  * Created by qyh on 2019/2/18.
  */
 public interface IOrderService {
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+
+    ServerResponse aliCallback(Map<String,String> params);
+
+    ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
 }
