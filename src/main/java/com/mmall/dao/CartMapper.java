2311a935187ff6abbdeb944ef24a1ddb4d55e8d4
QCloudHao <499930858@qq.com>
2019-02-20 14:29:59 +0800

    complete Order Management Module

--- src/main/java/com/mmall/dao/CartMapper.java
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -30,4 +30,6 @@ public interface CartMapper {
                                   @Param("checked") Integer checked);
 
     int selectCartProductCount(Integer userId);
+
+    List<Cart> selectCheckedCartByUserId(Integer userId);
 }
